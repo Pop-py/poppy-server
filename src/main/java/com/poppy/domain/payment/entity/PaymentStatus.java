df0abb09a42a70eebc7627e575efbc93b8868095
Hxxjeong <heejeong3394@gmail.com>
2024-12-04 16:53:49 +0900

    [Refactor]: PaymentStatus description 추가

--- src/main/java/com/poppy/domain/payment/entity/PaymentStatus.java
+++ src/main/java/com/poppy/domain/payment/entity/PaymentStatus.java
@@ -1,8 +1,14 @@
 package com.poppy.domain.payment.entity;
 
 public enum PaymentStatus {
-    PENDING,    // 대기
-    DONE,   // 완료
-    FAILED, // 실패
-    CANCELED    // 취소
+    PENDING("대기"),
+    DONE("완료"),
+    FAILED("실패"),
+    CANCELED("취소");
+
+    private final String description;
+
+    PaymentStatus(String description) {
+        this.description = description;
+    }
 }
