abeebe63041855c99753519c2fad8713ba1606cc
Hxxjeong <heejeong3394@gmail.com>
2024-12-04 16:59:31 +0900

    [Refactor]: ReservationStatus description 추가

--- src/main/java/com/poppy/domain/reservation/entity/ReservationStatus.java
+++ src/main/java/com/poppy/domain/reservation/entity/ReservationStatus.java
@@ -1,7 +1,13 @@
 package com.poppy.domain.reservation.entity;
 
 public enum ReservationStatus {
-    PENDING,
-    CHECKED,
-    CANCELED
+    PENDING("대기"),
+    CHECKED("예약 완료"),
+    CANCELED("예약 취소");
+
+    private final String description;
+
+    ReservationStatus(String description) {
+        this.description = description;
+    }
 }
