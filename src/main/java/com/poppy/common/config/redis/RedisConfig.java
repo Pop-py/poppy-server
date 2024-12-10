d6db36958c156cdabcdb2b92d2855b5796164fb0
Hxxjeong <heejeong3394@gmail.com>
2024-12-10 11:09:18 +0900

    [Build]: Redis 비밀번호 설정

--- src/main/java/com/poppy/common/config/redis/RedisConfig.java
+++ src/main/java/com/poppy/common/config/redis/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -25,7 +26,12 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setPassword(password);
+
+        return new LettuceConnectionFactory(configuration);
     }
 
     @Bean
