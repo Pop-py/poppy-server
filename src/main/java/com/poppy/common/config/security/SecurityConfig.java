6add659379787fad6d34fd1aa33830522b2efc5f
Hxxjeong <heejeong3394@gmail.com>
2025-01-08 14:44:30 +0900

    [Fix]: 로그인한 유저만 최근 본 팝업 스토어 조회하도록 변경

--- src/main/java/com/poppy/common/config/security/SecurityConfig.java
+++ src/main/java/com/poppy/common/config/security/SecurityConfig.java
@@ -47,7 +47,6 @@ public class SecurityConfig {
                         // 팝업스토어 관련
                         .requestMatchers("/popup-stores/**").permitAll()
                         .requestMatchers("/search-history/popular").permitAll()
-                        .requestMatchers("/users/recent").permitAll()
                         // 공지사항 관련
                         .requestMatchers("/notices/**").permitAll()
                         // 리뷰 관련
