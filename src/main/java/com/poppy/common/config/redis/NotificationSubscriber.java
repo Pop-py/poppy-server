d049ba55c8d3bdd54fc1b99c2798b3bf47eb0345
oniwon <cookie89800@naver.com>
2025-02-08 15:53:12 +0900

    [Feat]: 공지사항 WebSocket 알림 경로 추가

--- src/main/java/com/poppy/common/config/redis/NotificationSubscriber.java
+++ src/main/java/com/poppy/common/config/redis/NotificationSubscriber.java
@@ -33,18 +33,27 @@ public class NotificationSubscriber implements MessageListener {
 
     private void sendWebSocketNotification(NotificationDto notification) {
         try {
+            // 특정 사용자에게 알림 전송
             String destination = "/queue/notifications";
             messagingTemplate.convertAndSendToUser(
                     String.valueOf(notification.getUserId()), // userId를 username으로 사용
                     destination,
                     notification
             );
-            log.info("WebSocket notification sent - message: {}, type: {}, userId: {}, popupStoreName: {}, isRead: {}",
+            log.info("개인 알림 전송 - message: {}, type: {}, userId: {}, popupStoreName: {}, isRead: {}",
                     notification.getMessage(),
                     notification.getType(),
                     notification.getUserId(),
                     notification.getPopupStoreName(),
                     notification.getIsRead());
+
+            // 모든 사용자에게 공지사항 전송 (공지사항인 경우)
+            if (notification.getType().name().equals("NOTICE")) {
+                String globalDestination = "/topic/notifications";
+                messagingTemplate.convertAndSend(globalDestination, notification);
+                log.info("공지사항 전송 - message: {}", notification.getMessage());
+            }
+
         } catch (Exception e) {
             log.error("Failed to send WebSocket notification: {}", e.getMessage(), e);
         }
