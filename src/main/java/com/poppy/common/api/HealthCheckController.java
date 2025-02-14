30832e581a2129d6494ddb8ea88bc0683ee2b40d
Hxxjeong <heejeong3394@gmail.com>
2025-02-14 14:56:42 +0900

    [Refactor]: 헬스체크 DB 확인하도록 변경

--- src/main/java/com/poppy/common/api/HealthCheckController.java
+++ src/main/java/com/poppy/common/api/HealthCheckController.java
@@ -1,14 +1,28 @@
 package com.poppy.common.api;
 
+import com.poppy.domain.popupStore.entity.PopupStore;
+import com.poppy.domain.popupStore.repository.PopupStoreRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthCheckController {
+    private final PopupStoreRepository popupStoreRepository;
+
     @GetMapping("/")
     public ResponseEntity<String> healthCheck() {
-        return new ResponseEntity<>("OK", HttpStatus.OK);
+        try {
+            String storeName = popupStoreRepository.findById(1L)
+                    .map(PopupStore::getName)
+                    .orElse("Store not found");
+
+            return new ResponseEntity<>("OK - DB Connected: " + storeName, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("DB Connection Failed: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
